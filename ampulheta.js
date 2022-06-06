1e765a94f988760da40d54aa37e4c9ea86e244df
medriel <adriel.souza397@gmail.com>
2022-06-06 20:03:53 -0300

    Input capture and validation

--- ampulheta.js
+++ ampulheta.js
@@ -1,1 +1,13 @@
-console.log("Ampulheta")
+function inputCapture() {
+  const inputValue = prompt("Informe um número a partir de 20 para o tamanho da ampulheta:");
+  inputValidation(inputValue);
+}
+
+function inputValidation(value) {
+  if (value < 20) {
+    alert("O número digitado deve ser a partir de 20");
+    inputCapture();
+  }
+}
+
+inputCapture();
